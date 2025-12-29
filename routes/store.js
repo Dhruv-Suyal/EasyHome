c017b1ad24a64a94c66ab8d7d3dc189004cc738a
Dhruv-Suyal <dhruvsuyal0@gmail.com>
2025-12-29 20:16:25 +0530

    EasyHome done

--- routes/store.js
+++ routes/store.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {homeList, getbooking,getIndex,getFavouriteList, homeDetails, postFavouriteList, deleteFavourite, postbooking, getAllBookings, postCancelBooking, getbookinghistory, getAutoComplete, getSearch, terms, contact, privacyPolicy, cookiePolicy} = require('../controller/store');
+const {homeList, getbooking,getIndex,getFavouriteList, homeDetails, postFavouriteList, deleteFavourite, postbooking, getAllBookings, postCancelBooking, getbookinghistory, getAutoComplete, getSearch, terms, contact, privacyPolicy, cookiePolicy, desclaimer, faq, helpCenter, safetyTips} = require('../controller/store');
 
 const storeRouter = express.Router();
 
@@ -20,5 +20,9 @@ storeRouter.get("/terms", terms);
 storeRouter.get("/contact", contact);
 storeRouter.get("/privacyPolicy", privacyPolicy);
 storeRouter.get("/cookiePolicy", cookiePolicy);
+storeRouter.get("/desclaimer", desclaimer);
+storeRouter.get("/faq", faq);
+storeRouter.get("/helpCenter",helpCenter);
+storeRouter.get("/safetyTips", safetyTips);
 
 module.exports = storeRouter;
